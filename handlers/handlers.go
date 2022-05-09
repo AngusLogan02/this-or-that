b6ed1803286a8e0d25d9f60f1d9d7e6531244ae0
Angus Logan <angus.logan@baronsview.com>
2022-05-09 21:19:35 +0100

    dynamically generate things related to vidya game

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"this-or-that/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func IndexHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{})
+	this, that := middlewares.GetOptions("Video%20game")
+	fmt.Println("this", this)
+	fmt.Println("that", that)
+	c.HTML(http.StatusOK, "index.html", gin.H{"this": this, "that": that})
 }
---
+++ middlewares/middlewares.go
@@ -0,0 +1,83 @@
+package middlewares
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Response struct {
+	Continue Continue `json:"continue"`
+	Query    Query    `json:"query"`
+}
+type Continue struct {
+	Plcontinue string `json:"plcontinue"`
+	Continue   string `json:"continue"`
+}
+type Links struct {
+	Ns    int    `json:"ns"`
+	Title string `json:"title"`
+}
+type ID struct {
+	PageID int     `json:"pageid"`
+	NS     int     `json:"ns"`
+	Title  string  `json:"title"`
+	Links  []Links `json:"links"`
+}
+type Pages struct {
+	ID ID
+}
+type Query struct {
+	Pages map[int]json.RawMessage
+}
+
+func GetOptions(category string) (this string, that string) {
+	reqUrl := "https://en.wikipedia.org/w/api.php?action=query&titles=" + category + "&prop=links&pllimit=max&format=json"
+
+	fmt.Println(reqUrl)
+
+	res, err := http.Get(reqUrl)
+
+	if err != nil {
+		log.Fatal("Error retrieving response from wikipedia API", err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatal("Error reading response body", err)
+	}
+
+	var response Response
+
+	json.Unmarshal(body, &response)
+
+	var key int
+	for k, _ := range response.Query.Pages {
+		key = k
+	}
+
+	var bruh ID
+	json.Unmarshal(response.Query.Pages[key], &bruh)
+	// fmt.Println(response.Query.Pages)
+
+	var options [500]string
+	for i, val := range bruh.Links {
+		options[i] = val.Title
+	}
+
+	rand.Seed(time.Now().Unix())
+	rand1 := rand.Intn(500)
+	rand2 := rand.Intn(500)
+	for rand1 == rand2 {
+		rand2 = rand.Intn(500)
+	}
+	fmt.Println(options)
+	return options[rand1], options[rand2]
+}
